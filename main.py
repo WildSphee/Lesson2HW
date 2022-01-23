7bbd410f89809193e78c2be0510cc2ae5ae8aabd
WildSphee <38903045+WildSphee@users.noreply.github.com>
2022-01-23 23:38:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,27 +10,6 @@
 #     print("Error, type a number")
 #
 
-#Exercise 2
-#
-# userinput = input("Type a series of numbers").split()
-# try:
-#     userinput = list(map(int, userinput))
-#
-#     factors = []
-#     for i in range(1, min(userinput)+1):
-#         assumption = True
-#         for num in userinput:
-#             if num % i != 0:
-#                 assumption = False
-#         if assumption:
-#             factors.append(i)
-#
-#     print(factors)
-#
-# except:
-#     print("please ensure all inputs are numbers")
-
-
 # Exercise 2
 
 userinput = input("Type a series of numbers").split()
@@ -39,13 +18,15 @@ try:
 
     factors = []
     for i in range(1, min(userinput)+1):
-        # assumption = True
+        assumption = True
         for num in userinput:
             if num % i != 0:
-                break
+                assumption = False
+        if assumption:
             factors.append(i)
 
     print(factors)
 
 except:
     print("please ensure all inputs are numbers")
+
